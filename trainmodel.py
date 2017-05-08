ff4a7da0980bd9d9866ae6fd088a0b75c0b48702
Priyanjit Dey <priyanjitdey94@users.noreply.github.com>
2017-05-08 21:03:09 +0530

    Training an SVM using the labelled eye patches.

---
+++ trainmodel.py
@@ -0,0 +1,101 @@
+#Class responsible for SVM training
+
+import Image;
+import glob;
+import sys;
+import os;
+import cv2;
+import time;
+import argparse;
+import itertools;
+import numpy as np;
+
+np.set_printoptions(precision=2)
+fileDir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(fileDir, ".."))
+
+import openface
+import openface.helper
+from openface.data import iterImgs
+
+# Path to include pre-trained models
+modelDir = os.path.join(fileDir, '..', 'models')
+dlibModelDir = os.path.join(modelDir, 'dlib')
+openfaceModelDir = os.path.join(modelDir, 'openface')
+
+parser = argparse.ArgumentParser()
+
+# Path to dlib face predictor
+parser.add_argument('--dlibFaceMean', type=str, help="Path to dlib's face predictor.",
+                    default=os.path.join(dlibModelDir, "mean.csv"))
+parser.add_argument('--dlibFacePredictor', type=str, help="Path to dlib's face predictor.",
+                    default=os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
+parser.add_argument('--dlibRoot', type=str,
+                    default=os.path.expanduser(
+                        "~/src/dlib-18.16/python_examples"),
+                    help="dlib directory with the dlib.so Python library.")
+
+# Path to torch neural net
+parser.add_argument('--networkModel', type=str, help="Path to Torch network model.",
+                    default=os.path.join(openfaceModelDir, 'nn4.v1.t7'))
+
+# Specify image dimension. 96 in this case.
+parser.add_argument('--imgDim', type=int,
+                    help="Default image dimension.", default=96)
+
+# Specify whether to use CUDA
+parser.add_argument('--cuda', action='store_true')
+
+#Specify whether to use Verbose
+parser.add_argument('--verbose', action='store_true')
+
+args = parser.parse_args()
+
+sys.path.append(args.dlibRoot)
+import dlib
+
+from openface.alignment import NaiveDlib  # Depends on dlib.
+if args.verbose:
+    print("Argument parsing and loading libraries took {} seconds.".format(
+        time.time() - start))
+
+start = time.time()
+align = NaiveDlib(args.dlibFaceMean, args.dlibFacePredictor)
+if args.verbose:
+    print("Loading the dlib and OpenFace models took {} seconds.".format(
+        time.time() - start))
+
+class TrainModel:
+
+	def __init__(self):
+		self.modelName="defaultName";
+		self.modelLocation="/home/trainedModel";
+		self.modelFormat="pkl";
+
+	def getModelName(self):
+		return self.modelName;
+
+	def setModelName(self,name):
+		self.modelName=name;
+
+	def getModelLocation(self):
+		return self.modelLocation;
+
+	def setModelLocation(self,path):
+		self.modelLocation=path;
+
+	def getModelFormat(self):
+		return self.modelFormat;
+
+	def setModelFormat(self,format):
+		self.modelFormat=format;
+
+	def startTraining:
+		feat_x = np.load('/home/prithviraj/open_feature/of.npy');
+		feat_y= np.load('/home/prithviraj/open_feature/oflabel.npy');
+		print feat_x.shape, feat_y.shape;
+		print "Training...........";
+		clf = svm.SVC();
+		clf.fit(feat_x, feat_y);
+		joblib.dump(clf, '/home/prithviraj/open_feature/trained_svm.pkl',compress=1);
+		print "Training Complete.";
