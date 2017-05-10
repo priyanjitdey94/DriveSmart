62b2313d7509a6f01beec7aec9abce9da82bea12
Priyanjit Dey <priyanjitdey94@users.noreply.github.com>
2017-05-11 00:43:23 +0530

    Eye patch feature

--- TrainingSystemDriver.py
+++ TrainingSystemDriver.py
@@ -26,20 +26,14 @@ openfaceModelDir = os.path.join(modelDir, 'openface')
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--dlibFaceMean', type=str, help="Path to dlib's face predictor.",
-                    default=os.path.join(dlibModelDir, "mean.csv"))
-parser.add_argument('--dlibFacePredictor', type=str, help="Path to dlib's face predictor.",
-                    default=os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
-parser.add_argument('--dlibRoot', type=str,
-                    default=os.path.expanduser(
-                        "~/src/dlib-18.16/python_examples"),
-                    help="dlib directory with the dlib.so Python library.")
-parser.add_argument('--networkModel', type=str, help="Path to Torch network model.",
-                    default=os.path.join(openfaceModelDir, 'nn4.v1.t7'))
-parser.add_argument('--imgDim', type=int,
-                    help="Default image dimension.", default=96)
+parser.add_argument('--dlibFaceMean', type=str, help="Path to dlib's face predictor.",default=os.path.join(dlibModelDir, "mean.csv"))
+parser.add_argument('--dlibFacePredictor', type=str, help="Path to dlib's face predictor.",default=os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
+parser.add_argument('--dlibRoot', type=str,default=os.path.expanduser("~/src/dlib-18.16/python_examples"),help="dlib directory with the dlib.so Python library.")
+parser.add_argument('--networkModel', type=str, help="Path to Torch network model.",default=os.path.join(openfaceModelDir, 'nn4.v1.t7'))
+parser.add_argument('--imgDim', type=int,help="Default image dimension.", default=96)
 parser.add_argument('--cuda', action='store_true')
 parser.add_argument('--verbose', action='store_true')
+
 
 args = parser.parse_args()
 
@@ -48,15 +42,15 @@ import dlib
 
 from openface.alignment import NaiveDlib  # Depends on dlib.
 if args.verbose:
-    print("Argument parsing and loading libraries took {} seconds.".format(
-        time.time() - start))
+	print("Argument parsing and loading libraries took {} seconds.".format(
+		time.time() - start))
 
 start = time.time()
 align = NaiveDlib(args.dlibFaceMean, args.dlibFacePredictor)
 net = openface.TorchWrap(args.networkModel, imgDim=args.imgDim, cuda=args.cuda)
 if args.verbose:
-    print("Loading the dlib and OpenFace models took {} seconds.".format(
-        time.time() - start))
+	print("Loading the dlib and OpenFace models took {} seconds.".format(
+		time.time() - start))
 
 
 ###################################################################################
@@ -70,11 +64,11 @@ TM=TrainModel();
 
 print "====== Welcome to DriveSmart Training Module ======";
 print "Enter the path to the video folder:";
-videoPath=input('--->');
+videoPath=raw_input('--->');
 print "Enter the text file containing list of videos with closed eyes:";
-closedTextPath=input('--->');
+closedTextPath=raw_input('--->');
 print "Enter the text file containing list of videos with open eyes:";
-openTextPath=input('--->');
+openTextPath=raw_input('--->');
 
 #Extracting Frames 
 FE.setVideoFileName(videoPath);
@@ -82,12 +76,12 @@ FE.fetchFrame(closedTextPath,0);
 FE.fetchFrame(openTextPath,1);
 
 #Extracting Eye Patches
-LF.getEyePatch(videoPath+"/Frames_0");
-LF.getEyePatch(videoPath+"/Frames_1");
+#LF.getEyePatch(videoPath+"/Frames_0");
+#LF.getEyePatch(videoPath+"/Frames_1");
 
 #Labelling Eye Patches
-LF.labelFrame(videoPath+"/Frames_0_eye_patch",0);
-LF.labelFrame(videoPath+"/Frames_1_eye_patch",1);
+LF.labelFrame(videoPath+"/Frames_0_wide_eye_patch",0);
+LF.labelFrame(videoPath+"/Frames_1_wide_eye_patch",1);
 
 #Training Model
 TM.startTraining();
